212edb0fbceb621029976e9d467e293251562cd4
Lennox Bampoe-Addo <LBampoe-Addo@spartaglobal.com>
2019-10-11 14:24:40 +0100

    Hi I created this

---
+++ gitit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+git_go(){
+  git add .
+  echo "Enter the commit message:"
+  read commitMessage
+
+  git commit -m "$commitMessage"
+
+  echo "Enter name of branch"
+  read branch
+
+  git push origin $branch
+  read
+}
+
+git_go
